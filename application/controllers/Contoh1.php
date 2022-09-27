30f81ff1ad3d7461d86aa7987c115cb6074c6ae4
rafaalfito <alfito967@gmail.com>
2022-09-27 13:58:51 +0700

    tugas pertemuan 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+	{
+	    echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Rafa Alfito
+		     Saya tinggal di daerah pamulang
+			 olahraga yang saya sukai adalah
+   			 tennis";
+    }
+}
